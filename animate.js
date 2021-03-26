f11d74df0e166439907a3b8dbe044b9830625db0
JerryZhongJ <zhong20000905@qq.com>
2021-03-26 22:16:50 +0800

    Upload by PicGo
    初见雏形
    
    忘记删除文件

---
+++ animate.js
@@ -0,0 +1,9 @@
+// $(".img-box").hover(function () {
+//     $(this).children("img").animate({
+//         height: "75%"
+//     });
+// }, function () {
+//     $(this).children("img").animate({
+//         height: "100%"
+//     });
+// });
