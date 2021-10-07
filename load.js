7b75afd2c46d71aaa5f0dab06856442c46bbb48b
Jerry Zhong <zhong20000905@qq.com>
2021-10-08 01:40:27 +0800

    info bar

--- load.js
+++ load.js
@@ -7,10 +7,13 @@ fetch("./metadata.json").then(function (response) {
     let pictures = metadata.pictures;
     for (let i = pictures.length - 1; i >= 0; i--){
         let imgWidth = pictures[i].width * imgHeight / pictures[i].height;
-        let filename = pictures[i].name;
-        $("main").append('<div class="wrapper" style="width: ' + imgWidth + 'px">\
+        $("main").append('<div class="wrapper" style="--width: ' + imgWidth + 'px">\
                             <div class="img-panel">\
-                                <img class="picture" src="pictures/' + filename + '" imgID=' + i + '>\
+                                <img class="picture" src="pictures/' + pictures[i].name + '" imgID=' + i + '>\
+                                <div class="info-bar">\
+                                    <span class="info resolution">'+ pictures[i].width + 'x' + pictures[i].width + '</span>\
+                                    <span class="info date">'+ pictures[i].date + '</span>\
+                                </div>\
                                 <div class="tagbar"></div>\
                             </div >\
                         </div > ');
