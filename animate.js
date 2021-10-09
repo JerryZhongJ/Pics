8d523457d59bb95a7bad24affb6db0ee1ac8b852
Jerry Zhong <zhong20000905@qq.com>
2021-10-09 17:41:05 +0800

    add background

--- animate.js
+++
@@ -1,9 +0,0 @@
-// $(".img-box").hover(function () {
-//     $(this).children("img").animate({
-//         height: "75%"
-//     });
-// }, function () {
-//     $(this).children("img").animate({
-//         height: "100%"
-//     });
-// });
