deb8bd8db2e8d2025133c1f8c05f3d403b04b22c
Jerry Zhong <zhong20000905@qq.com>
2021-10-09 16:05:10 +0800

    better fullscreen

--- fullscreen.js
+++ fullscreen.js
@@ -3,51 +3,40 @@ function addFullScreen() {
     let time;
     $(".picture").mousedown(function () {
         let picture = $(this);
+        let imgUrl = picture.attr("src");
+        let imgHeight = picture.height();
+        let imgWidth = picture.width();
+        $("body").append('<div class="fullscreen-bg">\
+                                <img class="fullscreen-img" src="'+ imgUrl +'">\
+                            </div > ');
+        let fullscreenBG = $(".fullscreen-bg");
+        let fullscreenImg = $(".fullscreen-img");
+        fullscreenBG.css("visibility", "hidden");
+        let fixedTop = picture.offset().top - $(document).scrollTop();
+        let fixedLeft = picture.offset().left - $(document).scrollLeft();
+        fullscreenImg.css({
+            "--fixed-top": fixedTop+"px",
+            "--fixed-left": fixedLeft+"px",
+            "--imgHeight": imgHeight+"px",
+            "--imgWidth": imgWidth+"px"
+        });
         timer = setTimeout(function () {
-            let imgUrl = picture.attr("src");
-            let imgHeight = picture.height();
-            let imgWidth = picture.width();
-            let top = picture.offset().top - $(window).scrollTop();
-            let left = picture.offset().left - $(window).scrollLeft();
-            $("body").append('<div class="fullscreen-bg">\
-                                <img class="fullscreen-img" src="'+imgUrl+'">\
-                              </div > ');
-            let fullscreenBG = $(".fullscreen-bg");
-            fullscreenBG.css({
-                "z-index": 2,
-                "position": "fixed",
-                "height": imgHeight / 0.9,
-                "width": imgWidth / 0.9,
-                "left": left,
-                "top": top,
-                "background-color": "rgba(0,0,0,0.7)",
-                "backdrop-filter": "blur(5px)",
-                "display": "flex",
-                "justify-content": "center",
-                "align-items": "center"
-            }).animate({
-                "height": "100vh",
-                "width": "100vw",
-                "left": "0px",
-                "top": "0px"
-            }, 300);
-            $(".fullscreen-img").css({
-                "display": "block",
-                "max-height": "90%",
-                "max-width": "90%",
-            });
-            clearTimeout(timer);
-            picID = picture.attr("picID");
+           
+            // background css and animation is implemented in style.css.
+            fullscreenBG.css("visibility", "visible");
+            fullscreenBG.attr("appear", "appear");
+            fullscreenImg.attr("appear", "appear");
             fullscreenBG.click(function () {
                 console.log("click");
-                let top = picture.offset().top - $(window).scrollTop();
-                let left = picture.offset().left - $(window).scrollLeft();
-                fullscreenBG.animate({
-                    "height": imgHeight / 0.9,
-                    "width": imgWidth / 0.9,
-                    "left": left,
-                    "top": top
-                }, 300, "swing", fullscreenBG.remove.bind(fullscreenBG));
+                let fixedTop = picture.offset().top - $(document).scrollTop();
+                let fixedLeft = picture.offset().left - $(document).scrollLeft();
+                fullscreenImg.css({
+                    "--fixed-top": fixedTop+"px",
+                    "--fixed-left": fixedLeft+"px",
+                });
+                fullscreenBG.attr("appear", "");
+                fullscreenImg.attr("appear", "");
+                setTimeout(fullscreenBG.remove.bind(fullscreenBG), 400);
             })
         }, 600);
     });
